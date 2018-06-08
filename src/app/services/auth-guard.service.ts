d52de3aa4d1f79ab248de691b9b89c710c223ed7
Matias Figueredo <MFigueredo@aconcaguasf.com>
2018-06-08 14:45:41 -0300

    Aplicacion que implemeta Auth0

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router,
+        ActivatedRouteSnapshot, // con este elemento puedo saber a donde esta intentando ingresar
+        RouterStateSnapshot,    // con este elemento puedo saber a donde esta intentando ingresar
+        CanActivate
+      } from '@angular/router';
+import { AuthService } from './auth.service';
+import { RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor( private auth: AuthService) { }
+
+  canActivate( next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    console.log(next);
+
+    if( this.auth.isAuthenticated() ) {
+      console.log('Pasó el guard');
+      return true;
+    } else {
+      console.error('Bloqueado por el guard');
+      return false;
+    }
+  }
+}
